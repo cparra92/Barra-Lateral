726258d0c854d4f10ffc7b3eebcec6a2de0b7b35
Cristian Parra <cristianparra992@gmail.com>
2016-10-07 16:25:24 -0300

    4 commint

--- app/src/main/java/com/example/cristian/barralateral/MainActivity.java
+++ app/src/main/java/com/example/cristian/barralateral/MainActivity.java
@@ -133,9 +133,10 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } //else if (id == R.id.nav_send) {
 
-        }
+        //}
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
